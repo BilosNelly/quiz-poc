73ac54a19d3451b6c424a5ee19ce5e8e3daf404d
Antonela Biloš <Antonela.Bilos@infobip.com>
2022-10-11 18:51:54 +0200

    Created questions screen and removed some items

--- src/App.jsx
+++ src/App.jsx
@@ -26,30 +26,29 @@ export const App = () => {
   },[])
 
   return (
-    <div className="on-quiz">
+//     <div className="on-quiz">
+//  MAIN PAGE
+//     <div className="on-quiz__content">
+//         <h1 className="on-quiz__content__title">Take the quiz and try your first pair!</h1>
+//         <button className="on-quiz__content__btn" type="button">Try On Trial!</button>
+//         <span className="on-quiz__content__copy">30 Days risk free</span>
+//     </div> 
+//     </div>
 
+        <div className="on-quiz__question-screen">
+            <div className="on-quiz__question-screen__header">
+                <span>Try on quiz 30 days risk free</span>
+            </div>
 
-      <div className="on-quiz__content">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-       
-            <h1 className="on-quiz__content__title">Take the quiz and try your first pair!</h1>
-            <button className="on-quiz__content__btn" type="button">Try On Trial!</button>
-            <span className="on-quiz__content__copy">30 Days risk free</span>
-     
+            <div className="on-quiz__question-screen__question">
+                <span>Are you male or female</span>
+            </div>
 
-      </div>
-    </div>
+            <div className="on-quiz__question-screen__actions">
+                <button>Male</button>
+                <button>Female</button>
+            </div>
+        </div>
   );
 }
 
