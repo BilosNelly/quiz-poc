7dbcb7cfdf28861581f346e79e15341073d459e9
Antonela Biloš <Antonela.Bilos@infobip.com>
2022-10-12 11:18:34 +0200

    Result screen added

--- src/App.jsx
+++ src/App.jsx
@@ -35,20 +35,41 @@ export const App = () => {
 //     </div> 
 //     </div>
 
-        <div className="on-quiz__question-screen">
-            <div className="on-quiz__question-screen__header">
-                <span>Try on quiz 30 days risk free</span>
-            </div>
+// SECOND PAGE
+//         <div className="on-quiz__question-screen">
+//             <div className="on-quiz__question-screen__header">
+//                 <span>Try on quiz 30 days risk free</span>
+//             </div>
+//             <div className="on-quiz__question-screen__question">
+//                 <span>Are you male or female</span>
+//             </div>
+//             <div className="on-quiz__question-screen__actions">
+//                 <button>Male</button>
+//                 <button>Female</button>
+//             </div>
+//         </div>
 
-            <div className="on-quiz__question-screen__question">
-                <span>Are you male or female</span>
-            </div>
-
-            <div className="on-quiz__question-screen__actions">
-                <button>Male</button>
-                <button>Female</button>
-            </div>
-        </div>
+      <div className="on-quiz__result__wrapper">
+          <div className="on-quiz__result">
+              <span className="on-quiz__result__title">Congratulations!</span>
+              <span className="on-quiz__result__description">Based on your selection we've decided on the Cloudventure and Cloudflyer! Enjoy the 30 day trial!</span>
+          </div>
+          <div className="on-quiz__result__shoe__wrapper">
+              <div className="on-quiz__result__shoe__img">here will be img</div>
+              <div className="on-quiz__result__shoe">
+                  <span className="on-quiz__result__title">Cloud X</span>
+                  <span className="on-quiz__result__description">You perfect partner in the world's lighest fully-cushioned shoe for Running Remixed.</span>
+              </div>
+              <div className="on-quiz__result__shoe__info">
+                  <span className="on-quiz__result__shoe__info__price">200CHF</span>
+                  <span className="on-quiz__result__shoe__info__separator" />
+                  <span>Neon and Grey</span>
+              </div>
+          </div>
+          <div className="on-quiz__result__btn">
+              <button>Shop now</button>
+          </div>
+      </div>
   );
 }
 
